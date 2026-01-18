9d643e0313666e82ca2f39975d340845b67ee693
venuparisipogu-3181 <venuparisipogu@gmail.com>
2026-01-18 22:53:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
+# API Credentials
+DHAN_TOKEN = "ఇక్కడ_మీ_DHAN_TOKEN"
+TELEGRAM_TOKEN = "ఇక్కడ_మీ_BOT_TOKEN"
+CHAT_ID = "ఇక్కడ_మీ_CHAT_ID"
 
+# Indices to Monitor
+INDICES = {
+    "NSE": ["NIFTY", "BANKNIFTY", "FINNIFTY"],
+    "BSE": ["SENSEX"]
+}
+
+# Strategy Constants
+DELTA_CE = (0.25, 0.50)
+DELTA_PE = (-0.50, -0.25) 
+OI_MIN = {"NIFTY": 500000, "BANKNIFTY": 200000, "FINNIFTY": 300000, "SENSEX": 100000}
